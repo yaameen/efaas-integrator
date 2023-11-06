c351a9d90402a487a9a4962d82e02bcf2764e56a
Iffam <iffaam@gmail.com>
2023-11-06 20:57:18 +0500

    adopt changes

--- src/Parsers/eFaasParser.php
+++ src/Parsers/eFaasParser.php
@@ -23,25 +23,25 @@ class eFaasParser
         $response = json_decode($object);
 
         // assuming if the returned payload includes the name, the rest of the profile would exist.
-        if (!property_exists($response, 'name')) {
+        if (!property_exists($response, 'full_name')) {
             throw new AccessDeniedToProfileException("You must provide access to your profile on eFaas");
         }
 
         return new Collection([
             'id' => $response->sub,
-            'name' => $response->name,
-            'name_dv' => str_replace('  ', ' ', $response->fname_dhivehi . ' ' . $response->mname_dhivehi . ' ' . $response->lname_dhivehi),
+            'name' => $response->full_name,
+            'name_dv' => $response->full_name_dhivehi,
             'gender' => $genders[$response->gender] ?? 'other',
             'date_of_birth' => Carbon::parse($response->birthdate),
             'identity_number' => $response->idnumber,
-            'phone_number' => $response->phone_number,
+            'phone_number' => $response->mobile,
             'email' => $response->email,
-            'user_state' => (int)$response->user_state,
-            'verification_level' => (int)$response->verification_level,
-            'user_type' => (int)$response->user_type,
+            // 'user_state' => (int)$response->user_state,
+            'verification_level' => 1,
+            'user_type' => $response->user_type_description == 'Maldivian' ? 1 : 0,
             'is_workpermit_active' => $response->is_workpermit_active == 'True',
             'passport_number' => $response->passport_number,
-            'permanent_address' => json_decode($response->address),
+            'permanent_address' => json_decode($response->permanent_address),
             'current_address' => '',
         ]);
     }
